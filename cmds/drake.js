88d53ba3c60a5660c03bb32fcb5338ff3388618d
Helringo <37148032+Helringo@users.noreply.github.com>
2018-03-07 19:29:41 +0100

    Update drake.js

--- cmds/drake.js
+++ cmds/drake.js
@@ -1,13 +1,15 @@
 module.exports.run = async (bot, message, args) => {
 	let msg = await message.channel.send("Preparing Meme");
-
-	let memetext = args.join("_");
-	let memelink = `https://memegen.link/mordor/one_does_not_simply/${memetext}.jpg`
+	let input = args.join("_");
+	let liner = input.split('|');
+	let line1 = liner[0];
+	let line2 = liner[1];
+	let memelink = `https://memegen.link/drake/${line1}/${line2}.jpg`
 	await message.channel.send({files: [memelink]});
 
 	msg.delete();
 }
 
 module.exports.help = {
-	name: "mordor"
+	name: "drake"
 }
