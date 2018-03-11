abba0ed5a4c072bc8d4042f07757e7aaf98d8a19
Helringo <37148032+Helringo@users.noreply.github.com>
2018-03-11 20:23:27 +0100

    Update bless.js

--- cmds/bless.js
+++ cmds/bless.js
@@ -1,9 +1,9 @@
 module.exports.run = async (bot, message, args) => {
 	let msg = await message.channel.send("Preparing blessing~!");
 
-	await message.channel.send({files: ["https://i.imgur.com/yVoov1w.jpg"]});
+	await message.channel.send("Smolanta blesses all the sleeps :green_heart:", {files: ["https://i.imgur.com/yVoov1w.jpg"]});
 
-	msg.edit("Smolanta blesses all the sleeps :green_heart:");
+	msg.delete();
 }
 
 module.exports.help = {
