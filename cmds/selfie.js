8e5638cb028112e8c4e5aadcb2d4aed03be63604
Helringo <37148032+Helringo@users.noreply.github.com>
2018-03-24 18:58:11 +0100

    Create selfie.js

---
+++ cmds/selfie.js
@@ -0,0 +1,23 @@
+request = require('request');
+module.exports = {
+  exec: (client, message, content, args) => {
+    message.send("Surely, please wait a moment");
+    request({
+      url: 'https://safebooru.donmai.us/posts.json?random=true&limit=1&tags=atalanta_%28fate%29',
+      json: true
+    }, function (err, temp, body) {
+      if (!err && body.length > 0 && body[0].file_url) {
+        selfie = {
+          image: { url: "https://safebooru.donmai.us" + body[0].file_url },
+          description: "Here you go",
+          color: 0xff0000
+        }
+        message.channel.sendMessage("", { embed: selfie });
+      } else {
+        message.send("I'm sorry, the camera is broken");
+        console.log(err);
+        console.log(body);
+      }
+    });
+  }
+}
