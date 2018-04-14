32b7092f03b3400415eefdfe3376ff637ece493e
Helringo <37148032+Helringo@users.noreply.github.com>
2018-04-14 16:30:19 +0200

    Update thonk.js

--- cmds/thonk.js
+++ cmds/thonk.js
@@ -1,12 +1,27 @@
 module.exports.run = async (bot, message, args) => {
 	const thonk = bot.emojis.find("name", "remthonk");
-	const thonk2 = bot.emojis.find("name", "NatsukiTHONK");
-	const thonk4 = bot.emojis.find("name", "sayothonk");
-	const thonk5 = bot.emojis.find("name", "Thonk");
-	const thonk6 = bot.emojis.find("name", "hyperthonk");
-	const thonk7 = bot.emojis.find("name", "illyaTHONK");
-	const thonk8 = bot.emojis.find("name", "MordyThonk");
-   message.channel.send(`${thonk} ${thonk2} ${thonk4} ${thonk5} ${thonk6} ${thonk7} ${thonk8}`);
+	const thonk2 = bot.emojis.find("name", "natsukithonk");
+	const thonk3 = bot.emojis.find("name", "sayothonk");
+	const thonk4 = bot.emojis.find("name", "thonk");
+	const thonk5 = bot.emojis.find("name", "hyperthonk");
+	const thonk6 = bot.emojis.find("name", "illyathonk");
+	const thonk7 = bot.emojis.find("name", "mordythonk");
+	const thonk8 = bot.emojis.find("name", "berukathonk");
+	const thonk9 = bot.emojis.find("name", "robinmthonk");
+	const thonk10 = bot.emojis.find("name", "chromthonk");
+	const thonk11 = bot.emojis.find("name", "robinfthonk");
+	const thonk12 = bot.emojis.find("name", "reinhardtthonk");
+	const thonk13 = bot.emojis.find("name", "hectorthonk");
+	const thonk14 = bot.emojis.find("name", "ikethonk");
+	const thonk15 = bot.emojis.find("name", "ninothonk");
+	const thonk16 = bot.emojis.find("name", "feliciathonk");
+	const thonk17 = bot.emojis.find("name", "cordeliathonk");
+	const thonk18 = bot.emojis.find("name", "lukasthonk");
+	const thonk19 = bot.emojis.find("name", "arthurthonk");
+	const thonk20 = bot.emojis.find("name", "sorenthonk");
+   message.channel.send(`${thonk} ${thonk2} ${thonk3} ${thonk4} ${thonk5} ${thonk6} ${thonk7}
+			${thonk8} ${thonk9} ${thonk10} ${thonk11} ${thonk12} ${thonk13} ${thonk14}
+			${thonk15} ${thonk16} ${thonk17} ${thonk18} ${thonk19} ${thonk20}`);
 }
 module.exports.help = {
 	name: "thonk"
