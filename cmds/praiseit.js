aa8b10c675c517d505fe89fad5f5cdeb61c47b0b
Helringo <37148032+Helringo@users.noreply.github.com>
2018-05-30 00:38:18 +0200

    Update praiseit.js

--- cmds/praiseit.js
+++ cmds/praiseit.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 	if(!target){
 		return message.channel.send(`Solaire praises the sun ${praise}`);
 }else{
-	message.channel.send(`${praise} ${target} has praised the sun ${praise}`, {files: ["https://cdn.discordapp.com/emojis/451150472624078868.png?v=1"]});
+	message.channel.send(`${praise} ${target} has praised the sun${praise}`, {files: ["https://cdn.discordapp.com/emojis/451150472624078868.png"]});
 }
 }
 
