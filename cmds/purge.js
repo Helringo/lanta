2316961b1bb86ba0ce025578f42cd3a78b431caa
Helringo <37148032+Helringo@users.noreply.github.com>
2018-07-05 01:09:45 +0200

    Create purge.js

---
+++ cmds/purge.js
@@ -0,0 +1,9 @@
+module.exports.run = async (bot, message, args) => {
+	  message.delete();
+    const fetched = await message.channel.fetchMessages({limit: args[0]});
+    message.channel.bulkDelete(fetched)
+    .catch(error => message.channel.send(`Error: ${error}`));
+
+module.exports.help = {
+	name: "purge"
+}
