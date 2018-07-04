27c2009ae43171eb91ddfa365ddfdf1d7b550915
Helringo <37148032+Helringo@users.noreply.github.com>
2018-07-05 01:13:16 +0200

    Update purge.js

--- cmds/purge.js
+++ cmds/purge.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot, message, args) => {
 	  message.delete();
     const fetched = await message.channel.fetchMessages({limit: args[0]});
-    message.channel.bulkDelete(fetched)
+    message.channel.bulkDelete(fetched);
     //.catch(error => message.channel.send(`Error: ${error}`));
 
 module.exports.help = {
