aeceb876c0679d215aa2302e13ebab3af7fc74af
Helringo <37148032+Helringo@users.noreply.github.com>
2018-07-05 01:27:40 +0200

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -1,6 +1,10 @@
 module.exports.run = async (bot, message, args) => {
 	message.reply("I have sent the commands to your DMs.");
-  message.author.send("List of commands goes here.");
+  message.author.send("List of commands. <> is a required argument and [] an optional argument.",
+		     "All commands require the prefix a!",
+		     "----Moderation Commands----",
+		      "mute <user mention> - this mutes the mentioned user"
+		     );
 }
 
 module.exports.help = {
