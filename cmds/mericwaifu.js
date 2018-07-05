2f592d596c6f19d87973c8eef4ba37dc8278e06f
Helringo <37148032+Helringo@users.noreply.github.com>
2018-07-05 23:30:51 +0200

    Update mericwaifu.js

--- cmds/mericwaifu.js
+++ cmds/mericwaifu.js
@@ -10,8 +10,16 @@ message.channel.send("Surely, please wait a moment");
 	var waifArray = new Array();
 	waifArray[0] = 'scathach_%28fate%2Fgrand_order%29';
 	waifArray[1] = 'minamoto_no_raikou_%28fate%2Fgrand_order%29';
+	waifArray[2] = 'tamamo_%28fate%29_%28all%29';
+	waifArray[3] = 'patchouli_knowledge';
+	waifArray[4] = 'yakumo_ran';
+	waifArray[5] = 'yakumo_yukari';
+	waifArray[6] = 'nero_claudius_%28fate%29_%28all%29';
+	waifArray[7] = 'artoria_pendragon_%28all%29';
+	waifArray[8] = 'altera_%28fate%29';
+	
 
-let randowaif = Math.floor(Math.random() * 2);
+let randowaif = Math.floor(Math.random() * 9);
 let waiftag = waifArray[randowaif];
 let waifurl = `https://danbooru.donmai.us/posts.json?random=true&limit=1&tags=${waiftag}`;
     
