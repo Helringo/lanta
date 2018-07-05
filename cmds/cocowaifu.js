2bf855d3e48ca608a98746084fbb9e87ea26ebdc
Helringo <37148032+Helringo@users.noreply.github.com>
2018-07-05 22:15:05 +0200

    Create cocowaifu.js

---
+++ cmds/cocowaifu.js
@@ -0,0 +1,40 @@
+request = require("request");
+module.exports.run = async (bot, message, args) => {
+message.channel.send("Surely, please wait a moment");
+    
+    
+    function randomIntFromInterval(min,max)	
+{
+    Math.floor(Math.random()*(max-min+1)+min);
+}
+	var waifArray = new Array();
+	waifArray[0] = 'kiyohime_%28fate%2Fgrand_order%29';
+	waifArray[1] = 'minamoto_no_raikou_%28fate%2Fgrand_order%29';
+	waifArray[2] = 'm4_sopmod_ii_%28girls_frontline%29';
+
+let randowaif = Math.floor(Math.random() * 3);
+let waiftag = waifArray[randowaif];
+let waifurl = `https://danbooru.donmai.us/posts.json?random=true&limit=1&tags=${waiftag}`;
+    
+    
+    request({
+      url: waifurl,
+      json: true
+    }, function (err, temp, body) {
+      if (!err && body.length > 0 && body[0].file_url) {
+	      try{
+    message.channel.send("Here you go", {files: [body[0].file_url]});
+  } catch(error) {//here goes if someAsyncPromise() rejected}
+    return message.channel.send("Sorry, something went wrong.");
+  };    
+      } else {
+        message.channel.send("I'm sorry, the camera is broken");
+        console.log(err);
+        console.log(body);
+      }
+});
+}
+
+module.exports.help = {
+	name: "cocowaifu"
+}
