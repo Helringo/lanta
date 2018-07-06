899f0f2edd1374a97320858ea4403fa38d8c3862
Helringo <37148032+Helringo@users.noreply.github.com>
2018-07-06 21:56:24 +0200

    Create raffiawaifu.js

---
+++ cmds/raffiawaifu.js
@@ -0,0 +1,43 @@
+request = require("request");
+module.exports.run = async (bot, message, args) => {
+message.channel.send("Surely, please wait a moment");
+    
+    
+    function randomIntFromInterval(min,max)	
+{
+    Math.floor(Math.random()*(max-min+1)+min);
+}
+	var waifArray = new Array();
+	waifArray[0] = 'angelise_ikaruga_misurugi';
+	waifArray[1] = 'yaya_%28machine-doll%29';
+	waifArray[2] = 'satellizer_el_bridget';
+	waifArray[3] = 'izetta';
+	waifArray[4] = 'toujou_koneko';
+	waifArray[5] = 'rory_mercury';
+
+let randowaif = Math.floor(Math.random() * 6);
+let waiftag = waifArray[randowaif];
+let waifurl = `https://danbooru.donmai.us/posts.json?random=true&limit=1&tags=${waiftag}`;
+    
+    
+    request({
+      url: waifurl,
+      json: true
+    }, function (err, temp, body) {
+      if (!err && body.length > 0 && body[0].file_url) {
+	      try{
+    message.channel.send("Here you go", {files: [body[0].file_url]});
+  } catch(error) {//here goes if someAsyncPromise() rejected}
+    return message.channel.send("Sorry, something went wrong.");
+  };    
+      } else {
+        message.channel.send("I'm sorry, the camera is broken");
+        console.log(err);
+        console.log(body);
+      }
+});
+}
+
+module.exports.help = {
+	name: "raffiawaifu"
+}
