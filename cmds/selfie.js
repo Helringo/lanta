e4d27226f30a522489b9b1b9178834771986db12
Helringo <37148032+Helringo@users.noreply.github.com>
2018-07-06 02:34:30 +0200

    Update selfie.js

--- cmds/selfie.js
+++ cmds/selfie.js
@@ -2,7 +2,7 @@ request = require("request");
 module.exports.run = async (bot, message, args) => {
 message.channel.send("Surely, please wait a moment");
     request({
-      url: 'https://safebooru.donmai.us/posts.json?random=true&limit=1&tags=atalanta_%28fate%29',
+      url: 'https://danbooru.donmai.us/posts.json?random=true&limit=1&tags=atalanta_%28fate%29',
       json: true
     }, function (err, temp, body) {
       if (!err && body.length > 0 && body[0].file_url) {
