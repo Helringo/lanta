a6f030b32a604e65b511b580d7124d7992b236f3
Helringo <37148032+Helringo@users.noreply.github.com>
2018-08-04 23:11:15 +0200

    Update danbooru.js

--- cmds/danbooru.js
+++ cmds/danbooru.js
@@ -18,11 +18,12 @@ message.channel.send("Surely, please wait a moment");
       json: true
     }, function (err, temp, body) {
       if (!err && body.length > 0 && body[0].file_url) {
-	      try{
-    message.channel.send("Here you go", {files: [body[0].file_url]});
-  } catch(error) {//here goes if someAsyncPromise() rejected}
-    return message.channel.send("Sorry, something went wrong.");
-  };    
+	      danpost = {
+          image: { url: body[0].file_url },
+          description: "Here you go~",
+          color: 0x00B28C
+        }
+	message.channel.send("", { embed: danpost });
       } else {
         message.channel.send("I'm sorry, the camera is broken");
         console.log(err);
