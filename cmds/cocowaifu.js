db6cf7d21dd5c9499e09a26d5d08167f583188f4
Helringo <37148032+Helringo@users.noreply.github.com>
2018-08-16 01:14:54 +0200

    Update cocowaifu.js

--- cmds/cocowaifu.js
+++ cmds/cocowaifu.js
@@ -1,4 +1,5 @@
 request = require("request");
+const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 message.channel.send("Surely, please wait a moment");
     
@@ -35,12 +36,11 @@ let waifurl = `https://danbooru.donmai.us/posts.json?random=true&limit=1&tags=${
       json: true
     }, function (err, temp, body) {
       if (!err && body.length > 0 && body[0].file_url) {
-	  waifu = {
-          image: { url: body[0].file_url },
-          description: "Here you go~",
-          color: 0x00B28C
-        }
-	message.channel.send("", { embed: waifu });  
+	  let waifu = new Discord.RichEmbed()
+          .setImage(body[0].file_url)
+          .setDescription("Here you go~")
+          .setColor(0x00B28C)
+	message.channel.send({embed: waifu});  
       } else {
         message.channel.send("I'm sorry, the camera is broken");
         console.log(err);
