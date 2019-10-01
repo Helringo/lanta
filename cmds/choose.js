d7dc5dc0c5fa51ab84b1ea9acd4bcb53007525cc
Helringo <37148032+Helringo@users.noreply.github.com>
2019-10-01 19:41:28 +0200

    Update choose.js

--- cmds/choose.js
+++ cmds/choose.js
@@ -3,7 +3,8 @@ module.exports.run = async (bot, message, args) => {
 {
     Math.floor(Math.random()*(max-min+1)+min);
 }
-	let choices = Math.floor(Math.random() * args+1);
+	let length = args.length();
+	let choices = Math.floor(Math.random() * length+1);
 	let choice = args[choices];
 	if(!choice){
 		return message.channel.send("I can't choose nothing");
