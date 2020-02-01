6c1cf0e5a41ee800c6ccd8e54839689d3632f1ab
Helringo <37148032+Helringo@users.noreply.github.com>
2020-02-01 19:42:06 +0100

    Update dance.js

--- cmds/dance.js
+++ cmds/dance.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot, message, args) => {
 	let msg = await message.channel.send(":apple: Getting my apple ready :apple:");
 
-	await message.channel.send("Dance time~", {files: ["https://media.giphy.com/media/3o6fIRD9wEeSdRThSM/giphy.gif"]});
+	await message.channel.send("Dance time~", {files: ["https://img2.gelbooru.com/images/e8/ce/e8ceb06c1fdd39d9d04bf120ccd80912.gif"]});
 
 	msg.delete();
 }
